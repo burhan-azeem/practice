06ce9b26945d4750959ae15c5a46cbf3d2079ce3
Azeem <burhan.azeem@corp.ibexglobal.com>
2024-03-27 19:53:00 +0500

    config

--- practice/settings.py
+++ practice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-moxf1nz$a8g_wwoc$ld6wqz@14=o1loevnsll7cbpyl^ft&sm-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
